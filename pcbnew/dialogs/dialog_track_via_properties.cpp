fa0cafb39484ded7592f8bf73474f57954b951dd
Ben Ellis <benx45h@gmail.com>
2020-07-16 11:26:56 -0400

    Made net class width tracking checkbox account for layer

--- pcbnew/dialogs/dialog_track_via_properties.cpp
+++ pcbnew/dialogs/dialog_track_via_properties.cpp
@@ -456,7 +456,17 @@ bool DIALOG_TRACK_VIA_PROPERTIES::TransferDataFromWindow()
                     t->SetEnd( wxPoint( t->GetEnd().x, m_trackEndY.GetValue() ) );
 
                 if( m_trackNetclass->IsChecked() )
-                    t->SetWidth( t->GetNetClass()->GetTrackWidth() );
+                {
+                    if( t->IsOnInnerLayer() )
+                    {
+                        t->SetWidth( t->GetNetClass()->GetTrackWidthInner() );
+                    }
+                    else
+                    {
+                        t->SetWidth( t->GetNetClass()->GetTrackWidth() );
+                    }
+                }
+
                 else if( !m_trackWidth.IsIndeterminate() )
                     t->SetWidth( m_trackWidth.GetValue() );
 
