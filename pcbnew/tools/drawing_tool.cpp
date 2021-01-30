0ca870ec164a860f3ec4d170b48f65c3caee6635
Fabien Corona <f.corona@laposte.net>
2021-01-30 19:21:07 +0100

    formatting + move menu separator

--- pcbnew/tools/drawing_tool.cpp
+++ pcbnew/tools/drawing_tool.cpp
@@ -2,7 +2,7 @@
  * This program source code file is part of KiCad, a free EDA CAD application.
  *
  * Copyright (C) 2014-2017 CERN
- * Copyright (C) 2018-2020 KiCad Developers, see AUTHORS.txt for contributors.
+ * Copyright (C) 2018-2021 KiCad Developers, see AUTHORS.txt for contributors.
  * @author Maciej Suminski <maciej.suminski@cern.ch>
  *
  * This program is free software; you can redistribute it and/or
@@ -783,7 +783,7 @@ int DRAWING_TOOL::InteractivePlaceWithPreview( const TOOL_EVENT& aEvent, std::ve
 
             if( aLayers != NULL )
             {
-                PCB_LAYER_ID targetLayer = frame()->SelectLayer(
+                PCB_LAYER_ID targetLayer = frame()->SelectOneLayer(
                         PCB_LAYER_ID::PCB_LAYER_ID_COUNT, *aLayers, wxPoint( pos.x, pos.y ) );
 
                 view()->ClearPreview();
