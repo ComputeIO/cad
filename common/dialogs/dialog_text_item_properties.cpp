bc8bcf11afd42b3f4652b98e007c46f4a166dc4b
Ola Rinta-Koski <github@rinta-koski.net>
2021-05-12 16:03:46 +0300

    common base class between dialog_text_item_properties_base and dialog_{sch,pcb}_text_properties, intended for common functionality

---
+++ common/dialogs/dialog_text_item_properties.cpp
@@ -0,0 +1,26 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2021 Ola Rinta-Koski
+ * Copyright (C) 2021 KiCad Developers, see AUTHORS.txt for contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <dialogs/dialog_text_item_properties.h>
+
+DIALOG_TEXT_ITEM_PROPERTIES::DIALOG_TEXT_ITEM_PROPERTIES( wxWindow* aParent, EDA_TEXT* aText ) :
+        DIALOG_TEXT_ITEM_PROPERTIES_BASE( aParent ), m_text( aText )
+{
+}
---
+++ common/dialogs/dialog_text_item_properties.h
@@ -0,0 +1,38 @@
+/*
+ * This program source code file is part of KiCad, a free EDA CAD application.
+ *
+ * Copyright (C) 2021 Ola Rinta-Koski
+ * Copyright (C) 2021 KiCad Developers, see AUTHORS.txt for contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DIALOG_TEXT_ITEM_PROPERTIES_H
+#define DIALOG_TEXT_ITEM_PROPERTIES_H
+
+#include <dialogs/dialog_text_item_properties_base.h>
+
+class SCH_EDIT_FRAME;
+class EDA_TEXT;
+
+class DIALOG_TEXT_ITEM_PROPERTIES : public DIALOG_TEXT_ITEM_PROPERTIES_BASE
+{
+public:
+  DIALOG_TEXT_ITEM_PROPERTIES( wxWindow* aParent,
+                               EDA_TEXT* aText );
+protected:
+  EDA_TEXT* m_text;
+};
+
+#endif // DIALOG_TEXT_ITEM_PROPERTIES_H
