0db874515876238437d972a1669fa0d51a1a4b9b
Johan Grip <ogun@ogun.org>
2021-08-19 17:15:36 +0200

    Update action descriptions
    Reword the text and description of the 3D visibility
    actions to better match the wording in the footprint editor.
    E.g. changing 'Virtual' to 'Other'.

--- 3d-viewer/3d_viewer/tools/eda_3d_actions.cpp
+++ 3d-viewer/3d_viewer/tools/eda_3d_actions.cpp
@@ -210,19 +210,19 @@ TOOL_ACTION EDA_3D_ACTIONS::rotate45axisZ( "3DViewer.Control.rotate45axisZ",
 TOOL_ACTION EDA_3D_ACTIONS::showTHT( "3DViewer.Control.attributesTHT",
         AS_ACTIVE,
         'T', "",
-        _( "Toggle Through Hole 3D models" ), _( "Toggle 3D models with 'Through hole' attribute" ),
+        _( "Toggle Through Hole 3D models" ), _( "Toggle 3D models for 'Through hole' type components" ),
         BITMAPS::shape_3d, AF_NONE, (void*) FL_FP_ATTRIBUTES_NORMAL );
 
 TOOL_ACTION EDA_3D_ACTIONS::showSMD( "3DViewer.Control.attributesSMD",
         AS_ACTIVE,
         'S', "",
-        _( "Toggle SMD 3D models" ), _( "Toggle 3D models with 'Surface mount' attribute" ),
+        _( "Toggle SMD 3D models" ), _( "Toggle 3D models for 'Surface mount' type components" ),
         BITMAPS::icon_gerbview_24, AF_NONE, (void*) FL_FP_ATTRIBUTES_NORMAL_INSERT );
 
-TOOL_ACTION EDA_3D_ACTIONS::showVirtual( "3DViewer.Control.attributesVirtual",
+TOOL_ACTION EDA_3D_ACTIONS::showVirtual( "3DViewer.Control.attributesOther",
         AS_ACTIVE,
         'V', "",
-        _( "Toggle Virtual 3D models" ), _( "Toggle 3D models with 'Virtual' attribute" ),
+        _( "Toggle Other 3D models" ), _( "Toggle 3D models for 'Other' type components" ),
         BITMAPS::unknown, AF_NONE, (void*) FL_FP_ATTRIBUTES_VIRTUAL );
 
 TOOL_ACTION EDA_3D_ACTIONS::showBBoxes( "3DViewer.Control.showBoundingBoxes",
