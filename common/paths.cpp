KiCad/kicad-source-mirror

commit ad251b7b8c86ea3c0153fe823c3716db3f6d63ed

Simplify KICAD6_TEMPLATE_DIR default
The search_stack method is bugland

Fix https://gitlab.com/kicad/code/kicad/-/issues/10119

common/paths.cpp
@@ -239,6 +239,16 @@ wxString PATHS::GetStockScriptingPath()
 }
 
 
+wxString PATHS::GetStockTemplatesPath()
+{
+    wxString path;
+
+    path = GetStockDataPath() + wxT( "/template" );
+
+    return path;
+}
+
+
 wxString PATHS::GetStockPluginsPath()
 {
     wxFileName fn;

common/settings/common_settings.cpp
@@ -514,44 +514,7 @@ void COMMON_SETTINGS::InitializeEnvironment()
     path.AppendDir( wxT( "3dmodels" ) );
     addVar( wxT( "KICAD6_3DMODEL_DIR" ), path.GetFullPath() );
 
-    // We don't have just one default template path, so use this logic that originally was in
-    // PGM_BASE::InitPgm to determine the best default template path
-    {
-        // Attempt to find the best default template path.
-        SEARCH_STACK bases;
-        SEARCH_STACK templatePaths;
-
-        SystemDirsAppend( &bases );
-
-        for( unsigned i = 0; i < bases.GetCount(); ++i )
-        {
-            wxFileName fn( bases[i], wxEmptyString );
-
-            // Add KiCad template file path to search path list.
-            fn.AppendDir( "template" );
-
-            // Only add path if exists and can be read by the user.
-            if( fn.DirExists() && fn.IsDirReadable() )
-            {
-                wxLogTrace( tracePathsAndFiles, "Checking template path '%s' exists",
-                            fn.GetPath() );
-                templatePaths.AddPaths( fn.GetPath() );
-            }
-        }
-
-        if( templatePaths.IsEmpty() )
-        {
-            path = basePath;
-            path.AppendDir( "template" );
-        }
-        else
-        {
-            // Take the first one.  There may be more but this will likely be the best option.
-            path.AssignDir( templatePaths[0] );
-        }
-
-        addVar( wxT( "KICAD6_TEMPLATE_DIR" ), path.GetFullPath() );
-    }
+    addVar( wxT( "KICAD6_TEMPLATE_DIR" ), PATHS::GetStockTemplatesPath() );
 
     addVar( wxT( "KICAD_USER_TEMPLATE_DIR" ), PATHS::GetUserTemplatesPath() );
 

include/paths.h
@@ -122,6 +122,11 @@ public:
     static wxString GetStockDemosPath();
 
     /**
+     * Gets the stock (install) templates path
+     */
+    static wxString GetStockTemplatesPath();
+
+    /**
      * Gets the stock (install) 3d viewer plugins path
      */
     static wxString GetUserCachePath();

ComputeIO/cad

commit 496d0491e52a83bc18f8cacdfa42bcc19ef4e1ff

Fix stock templates path for flatpak
Commit ad251b7b8c86ea3c0153fe823c3716db3f6d63ed broke the template path
lookup for flatpak via `PATHS::GetStockTemplatesPath()`. Fix this by
using `::GetStockEDALibraryPath()` in all EDA Library related path
lookup functions.

`GetStockEDALibraryPath()` effectively does the same as
`GetStockDataPath()`, but it takes care to check the
`KICAD_LIBRARY_DATA` build config, which is being used by flatpak to
point to the flatpak EDA Library extensions in
`/app/extensions/Library/`.

Closes #11174

common/paths.cpp
@@ -203,7 +203,7 @@ wxString PATHS::GetStockSymbolsPath()
 {
     wxString path;
 
-    path = GetStockDataPath() + wxT( "/symbols" );
+    path = GetStockEDALibraryPath() + wxT( "/symbols" );
 
     return path;
 }
@@ -213,7 +213,7 @@ wxString PATHS::GetStockFootprintsPath()
 {
     wxString path;
 
-    path = GetStockDataPath() + wxT( "/footprints" );
+    path = GetStockEDALibraryPath() + wxT( "/footprints" );
 
     return path;
 }
@@ -223,7 +223,7 @@ wxString PATHS::GetStock3dmodelsPath()
 {
     wxString path;
 
-    path = GetStockDataPath() + wxT( "/3dmodels" );
+    path = GetStockEDALibraryPath() + wxT( "/3dmodels" );
 
     return path;
 }
@@ -243,7 +243,7 @@ wxString PATHS::GetStockTemplatesPath()
 {
     wxString path;
 
-    path = GetStockDataPath() + wxT( "/template" );
+    path = GetStockEDALibraryPath() + wxT( "/template" );
 
     return path;
 }