d84aaeca912b084e23633b757dfd72ffe6055bc9
lulu731 <lulu731lulu@hotmail.fr>
2022-06-21 22:46:33 +0200

    Implement dropping *.kicad_wks file to drawing sheet editor.

--- pagelayout_editor/files.cpp
+++ pagelayout_editor/files.cpp
@@ -315,3 +315,9 @@ bool PL_EDITOR_FRAME::SaveDrawingSheetFile( const wxString& aFullFileName )
 
     return false;
 }
+
+void PL_EDITOR_FRAME::DoWithAcceptedFiles()
+{
+    for( wxFileName file : m_AcceptedFiles )
+        OpenProjectFiles( { file.GetFullPath() }, KICTL_CREATE );
+}
