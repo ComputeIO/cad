4587f42fa1bb53380a07014804fec63284febc7c
lulu731 <lulu731lulu@hotmail.fr>
2022-06-21 15:54:48 +0200

    Refactor code by pushing code to EDA_BASE_FRAME level.

--- common/eda_base_frame.cpp
+++ common/eda_base_frame.cpp
@@ -1089,6 +1089,7 @@ void EDA_BASE_FRAME::OnPreferences( wxCommandEvent& event )
 #undef CREATE_PANEL
 }
 
+
 void EDA_BASE_FRAME::OnDropFiles( wxDropFilesEvent& aEvent )
 {
     wxString* files = aEvent.GetFiles();
@@ -1107,6 +1108,17 @@ void EDA_BASE_FRAME::OnDropFiles( wxDropFilesEvent& aEvent )
     DoWithAcceptedFiles();
     m_AcceptedFiles.clear();
 }
+
+
+void EDA_BASE_FRAME::DoWithAcceptedFiles()
+{
+    for( auto file : m_AcceptedFiles )
+    {
+        wxString fn = file.GetFullPath();
+        m_toolManager->RunAction( *m_acceptedExts.at( file.GetExt() ), true, &fn );
+    }
+}
+
 
 bool EDA_BASE_FRAME::IsWritable( const wxFileName& aFileName, bool aVerbose )
 {
