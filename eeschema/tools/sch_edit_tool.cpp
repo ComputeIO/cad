ccd03b1ff050f102c501c8aba2712fd480383cff
Mike Williams <mike@mikebwilliams.com>
2024-05-23 13:57:54 -0400

    design blocks: convert drag and dropped schematic to common code

--- eeschema/tools/sch_edit_tool.cpp
+++ eeschema/tools/sch_edit_tool.cpp
@@ -2733,8 +2733,7 @@ int SCH_EDIT_TOOL::EditPageNumber( const TOOL_EVENT& aEvent )
 
 int SCH_EDIT_TOOL::DdAppendFile( const TOOL_EVENT& aEvent )
 {
-    wxString aFileName = *aEvent.Parameter<wxString*>();
-    return ( m_frame->AddSheetAndUpdateDisplay( aFileName ) ? 0 : 1 );
+    return m_toolMgr->RunAction( EE_ACTIONS::importSheetContents, aEvent.Parameter<wxString*>() );
 }
 
 
